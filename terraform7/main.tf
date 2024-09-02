2af1baac33838e3fdfc950830d0e77bf7c6c4a16
Saideep Chakilam <saideep.chakilam@cloud4c.com>
2024-09-02 19:44:21 +0530

    kk

--- terraform7/main.tf
+++ terraform7/main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = ["value"]
   user_data = <<EOF
   #!/bin/bash
-  echo "Helloworld" > index.html
+  sudo apt-get install apache2 -y
+  echo "Helloworld" > /var/www/html/index.html
   nohup busybox httpd -f -p 8080 &
   EOF
 }
@@ -22,4 +23,4 @@ output "example_publicip" {
   value = aws_instance.example.public_ip
 }
 
-#sudo apt install apache2 -y
+#
